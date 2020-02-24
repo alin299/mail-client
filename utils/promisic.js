c8d51e30922acc82330a949bf301a191c5f95319
alin <823301694@qq.com>
2020-02-24 22:52:00 +0800

    init

---
+++ utils/promisic.js
@@ -0,0 +1,27 @@
+/**
+ * @name: promisic.js
+ * @author: alin
+ * @date: 2020-02-24 22:35
+ * @description：将任意函数promise化
+ * @update: 2020-02-24 22:35
+ */
+
+const pormisic = function (func) {
+    return function (params) {
+        return new Promise((resolve, reject) => {
+            Object.assign(params, {
+                success: (res) => {
+                  resolve(res)
+                },
+                fail: (error) => {
+                    reject(error)
+                }
+            })
+            func(params)
+        })
+    }
+}
+
+export {
+    pormisic
+}
